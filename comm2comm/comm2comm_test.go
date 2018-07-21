7ec77b412d765f8c077f6ce613248410c007982d
Jeff Hodges <jeff@somethingsimilar.com>
2018-07-20 22:09:24 -0700

    comm2comm: correct CommitteeID in test

--- comm2comm/comm2comm_test.go
+++ comm2comm/comm2comm_test.go
@@ -1,6 +1,10 @@
 package comm2comm
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/jmhodges/pac2pac/commparse"
+)
 
 func TestGolden(t *testing.T) {
 	trans, err := ParseFile("./itoth-test.txt")
@@ -12,11 +16,11 @@ func TestGolden(t *testing.T) {
 		t.Errorf("number of transactions: expected %d, got %d", expectedCount, len(trans))
 	}
 	tr := trans[len(trans)-1]
-	expectedSend := CommitteeID("C00012880")
+	expectedSend := commparse.CommitteeID("C00012880")
 	if tr.SendingCommittee != expectedSend {
 		t.Errorf("SendingCommittee: expected %#v, got %#v", expectedSend, tr.SendingCommittee)
 	}
-	expectedRecv := CommitteeID("C00428052")
+	expectedRecv := commparse.CommitteeID("C00428052")
 	if tr.ReceivingCommittee != expectedRecv {
 		t.Errorf("ReceivingCommittee: expected %#v, got %#v", expectedRecv, tr.ReceivingCommittee)
 	}
